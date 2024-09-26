7be1916a8ed93358c51a5faccf919cf4c6cbf89f
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-09-26 23:35:49 +0200

    hero and ComputersCanvas was created the 3d model with lighting was created and I works perfect.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
@@ -23,7 +23,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-        <ComputersCanvas />
+      <ComputersCanvas />
     </section>
   );
 };
