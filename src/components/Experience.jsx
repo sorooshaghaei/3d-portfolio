9a71abf073bb79c6a83a1d40704b75a54ae26d20
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-10-18 15:29:21 +0200

    init work experience by myself

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+import { motion } from "framer-motion";
+
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>What I have done so far</p>
+      <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
+  );
+};
 
-export default Experience
+export default SectionWrapper(Experience, "work");
