7b258be917cb8e4b5ee331750530210f62cc7111
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-10-25 23:53:01 +0200

    Experience component small update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -48,7 +48,7 @@ const ExperienceCard = ({ experience }) => (
       {experience.points.map((point, index) => (
         <li
           key={`experience-point-${index}`}
-          className="text-white-100 text-[14px] pl-1 tracking-wider"
+          className="text-white text-[14px] pl-1 tracking-wider"
         >
           {point}
         </li>
