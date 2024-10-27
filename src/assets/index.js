da4653dfbc2e5ddea8f18b02d631c46b89baa41c
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-10-27 14:06:05 +0100

    personalizing the icons and work experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -16,15 +16,10 @@ import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import blueWhite from "./company/blueWhite.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -48,14 +43,9 @@ export {
   mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
-  typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  blueWhite,
   carrent,
   jobit,
   tripguide,
