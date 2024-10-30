7aa0f0b0acc3673aa49c9202b756626f9e9fa6a3
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-10-30 23:57:37 +0100

    feedbacks init

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,23 +1,19 @@
-import { motion } from "framer-motion"
-import { testimonials } from "../constants"
-import { SectionWrapper } from "../hoc"
-import { styles } from "../styles"
+/* eslint-disable react-refresh/only-export-components */
+import { motion } from "framer-motion";
 
+import { testimonials } from "../constants";
 
-
-
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { textVariant } from "../utils/motion";
 
 const Feedbacks = () => {
   return (
-    <motion.div>
-     <p className={styles.sectionSubText}>
-what others say
-     </p>
-     <h2 className={styles.sectionHeadText}>
-Testimonials.
-     </h2>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>what others say</p>
+      <h2 className={styles.sectionHeadText}>Testimonials.</h2>
     </motion.div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Feedbacks,'')
+export default SectionWrapper(Feedbacks, "");
