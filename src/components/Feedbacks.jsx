ab6fdbf73d37bcc04ca38c24c6d39c46627233a8
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-10-30 23:59:22 +0100

    feedbacks init

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react-refresh/only-export-components */
 import { motion } from "framer-motion";
 
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
 import { testimonials } from "../constants";
 
-import { SectionWrapper } from "../hoc";
-import { styles } from "../styles";
-import { textVariant } from "../utils/motion";
 
 const Feedbacks = () => {
   return (
