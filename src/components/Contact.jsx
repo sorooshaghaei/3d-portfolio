c5493bf861ee6936090e438d8c76759f6e15aef1
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-11-01 13:09:44 +0100

    contact component init

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+/* eslint-disable react-refresh/only-export-components */
+import { useRef, useState } from 'react'
+
+import emailJs from '@emailjs/browser'
+
+import {motion} from 'framer-motion'
+import { SectionWrapper } from '../hoc'
+import { styles } from '../styles'
+
+import { EarthCanvas } from './canvas'
+import { slideIn } from '../utils/motion'
+
 
 const Contact = () => {
+
+  const formRef = useRef()
+
+  const [form,setForm]=useState({
+    name:'',
+    email:'',
+    message:''
+  })
+
+  const [loading,setLoading]=useState(false);
+
+  const handleChange = (e)=>{}
+  const handleSubmit = (e)=>{}
+
   return (
     <div>Contact</div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, 'contact') 
