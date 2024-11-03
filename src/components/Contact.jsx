878faf3faa2b4b39fa90d03480e328b243b6ffd4
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-11-03 01:27:12 +0100

    contact and Earth component semi done
    will implement the functionality of contact form later after the StarCanvas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
-      {/* form contact */}
+      {/* form contact on the left */}
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl " //flex-[0.75] ==> 3/4 of the screen
