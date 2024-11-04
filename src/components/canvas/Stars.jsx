e9cb6b9a76b77ba2d2f0c2c7b5fd577a5faf83e0
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-11-04 22:41:05 +0100

    now we can see stars
    and when page reload, they rotate.

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import { Suspense, useRef, useState } from "react";
 
@@ -8,17 +9,34 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
+  // sphere Generate random points in a spherical shape, with radius 1.2
+  // This creates a Float32Array of 5000 values, representing 3D coordinates (x, y, z)
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
   return (
-    <group>
-      <Points />
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled // Optimizes rendering by not drawing points outside the camera's view
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.0014}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
     </group>
   );
 };
 
 const StarsCanvas = () => {
   return (
-    <div className="w-full h-full absolute inset-0 z-[-1]">
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
