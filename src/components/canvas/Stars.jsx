0ab05324d1b96a56f61ecd387638984743de3f8e
sorooshaghaei <soroosh77aghaei@gmail.com>
2024-11-04 21:46:47 +0100

    StarsCanvas init

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -7,12 +7,11 @@ import { PointMaterial, Points, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
-  // const ref = useRef();
-  // const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  const ref = useRef();
 
   return (
     <group>
-      {/* <Points ref={ref} positions={sphere} /> */}
+      <Points />
     </group>
   );
 };
@@ -20,13 +19,13 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-full absolute inset-0 z-[-1]">
-      {/* <Canvas camera={{ position: [0, 0, 1] }}>
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
 
         <Preload all />
-      </Canvas> */}
+      </Canvas>
     </div>
   );
 };
